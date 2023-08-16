b2e12108e12aa7ed63e7537a8b6e836345a033f6
claytoncritchfield <clayton.critchfield@gmail.com>
2023-08-16 08:46:18 -0400

    added a link to the hero image inside the heroimg component

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,3 +2,12 @@ import React from "react";
 import Link from "next/link";
 import { useGlobalState } from "../context/GlobalState";
 import { useRouter } from "next/navigation";
+import 
+
+export default function Hero() {
+    return (
+        <>
+            <img src="https://github.com/DeveloperDadd/lootLegendsFrontend/blob/main/src/images/hero%20image.png?raw=true" />
+        </>
+    )
+}
