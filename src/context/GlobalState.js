0621e1beaa00295b1c019bea70a72bceb7d7f4b4
claytoncritchfield <clayton.critchfield@gmail.com>
2023-08-21 20:39:16 -0400

    updated globalState.js

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,29 +1,42 @@
-import { createContext, useReducer, useContext } from 'react';
-// Define the initial state
+import React, {
+  createContext,
+  useReducer,
+  useContext,
+  useEffect,
+} from 'react';
+
+import jwtDecode from 'jwt-decode'
+
+let user;
+
+if (typeof window !== 'undefined') {
+  user = JSON.parse(localStorage.getItem('user'))
+}
+
 const initialState = {
-  user: null,
-};
-// Create a context object
-const GlobalStateContext = createContext();
-// Reducer function to handle state changes
-function reducer(state, action) {
-  switch (action.type) {
-    case 'SET_USER':
-      return { ...state, user: action.payload };
-    default:
-      return state;
-  }
+  currentUser: user ? jwtDecode(user.access) : null,
+  currentUserToken: user ? user.access : null
 }
-// Provider component to wrap the app
-export function GlobalStateProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <GlobalStateContext.Provider value={{ state, dispatch }}>
-      {children}
-    </GlobalStateContext.Provider>
+
+const GlobalStateContext = createContext(initialState);
+const DispatchStateContext = createContext(undefined)
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(
+      (state, newValue) => ({ ...state, ...newValue }),
+      initialState
   );
+
+  return (
+      <GlobalStateContext.Provider value={state}>
+          <DispatchStateContext.Provider value={dispatch}>
+              {children}
+          </DispatchStateContext.Provider>
+      </GlobalStateContext.Provider>
+  )
 }
-// Custom hook to use the global state
-export function useGlobalState() {
-  return useContext(GlobalStateContext);
-}
+
+export const useGlobalState = () => [
+  useContext(GlobalStateContext),
+  useContext(DispatchStateContext)
+];
